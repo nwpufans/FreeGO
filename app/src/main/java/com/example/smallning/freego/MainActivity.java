fb1f3ecd15b86b1b129967f7f8d9e1633e27e6e5
nwpufans <524945775@qq.com>
2017-09-11 16:34:09 +0800

    加入Menu项
    新加入了Menu项，就左上角的菜单栏，底栏的按钮仍然有问题。

--- app/src/main/java/com/example/smallning/freego/MainActivity.java
+++ app/src/main/java/com/example/smallning/freego/MainActivity.java
@@ -7,8 +7,11 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -34,10 +37,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //底栏按钮
         mainButton=(Button)findViewById(R.id.mainButton);
         mapButton=(Button)findViewById(R.id.mapButton);
         societyButton=(Button)findViewById(R.id.societyButton);
         optionButton=(Button)findViewById(R.id.optionButton);
+        //注册按钮
         mainButton.setOnClickListener(this);
         mapButton.setOnClickListener(this);
         societyButton.setOnClickListener(this);
@@ -50,7 +55,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
+    //重写onCreateOptionsMenu方法
+    //加入menu模块
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.nav_menu,menu);
+        return true;
+    }
 
+    //menu响应模块
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,"You clicked item1",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item2:
+                Toast.makeText(this,"You clicked item2",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item3:
+                Toast.makeText(this,"You clicked item3",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item4:
+                Toast.makeText(this,"You clicked item4",Toast.LENGTH_LONG).show();
+                break;
+            default:;
+        }
+        return true;
+    }
 
     @Override
     public void onClick(View view) {
